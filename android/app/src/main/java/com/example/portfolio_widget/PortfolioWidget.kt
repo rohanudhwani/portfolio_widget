eb815858d76b732939d8855336c1b441bbe00f0e
Rohan Udhwani <rohanudhwani2@gmail.com>
2024-12-27 20:45:50 +0530

    CHANGE: ..

--- android/app/src/main/java/com/example/portfolio_widget/PortfolioWidget.kt
+++ android/app/src/main/java/com/example/portfolio_widget/PortfolioWidget.kt
@@ -14,10 +14,10 @@ class PortfolioWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
-        }
+//        // There may be multiple widgets active, so update all of them
+//        for (appWidgetId in appWidgetIds) {
+//            updateAppWidget(context, appWidgetManager, appWidgetId)
+//        }
     }
 
     override fun onEnabled(context: Context) {
@@ -29,16 +29,16 @@ class PortfolioWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.portfolio_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
-}
+//internal fun updateAppWidget(
+//    context: Context,
+//    appWidgetManager: AppWidgetManager,
+//    appWidgetId: Int
+//) {
+//    val widgetText = context.getString(R.string.appwidget_text)
+//    // Construct the RemoteViews object
+//    val views = RemoteViews(context.packageName, R.layout.portfolio_widget)
+//    views.setTextViewText(R.id.appwidget_text, widgetText)
+//
+//    // Instruct the widget manager to update the widget
+//    appWidgetManager.updateAppWidget(appWidgetId, views)
+//}
